8f19df989a721d8fcb510e6fd16ca3a11b3b8410
NUGGAN - NGGG <NuggaN85@users.noreply.github.com>
2015-12-12 21:31:03 +0100

    Update protectionantiplagiat.php

--- protectionantiplagiat.php
+++ protectionantiplagiat.php
@@ -41,12 +41,12 @@ $bannav = Array('HTTrack', 'HTTPClient', 'hors ligne', 'httpdown', 'Offline', 'P
 foreach ($bannav as $banned) {
     $comparaison = strstr($navigateur, $banned);
     if($comparaison!==false) {
-	    $write_this = '[Information] : '.$navigateur.'' . get_ip(); // Le texte que vous voulez avoir dans votre fichier stop.cnx
+	    $write_this = '[Information] : '.$navigateur.'' . get_ip(); // Le texte que vous voulez avoir dans votre fichier protectionantiplagiat.cnx
 		$tentative++;
 	}
 }
 if($tentative > 0){
-	$write_here = fopen("stop.cnx", "a"); // Fichier cnx auto inclus a la racine avec le stop.php
+	$write_here = fopen("protectionantiplagiat.cnx", "a"); // Fichier cnx auto inclus a la racine avec le protectionantiplagiat.php
 	fwrite($write_here, "\n" . $write_this);
 	fclose($write_here);
 	echo utf8_decode( '[Sécurité] Notre site web est protégé contre le vole et le spam, vos information sera automatiquement banni sur la base de donnée de projecthoneypot <br><br> [Information] : '.$navigateur.' '.get_ip().''); // Le texte que vous voulez que le voleur recevra dans les fichiers télécharger
