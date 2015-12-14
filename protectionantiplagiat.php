6e505dd39eb4adbdbe3a0dd35be800d6a9a12e03
NUGGAN - NGGG <NuggaN85@users.noreply.github.com>
2015-12-14 21:15:44 +0100

    Update protectionantiplagiat.php

--- protectionantiplagiat.php
+++ protectionantiplagiat.php
@@ -2,7 +2,7 @@
 /*!
  * protectionantiplagiat init v1.0
  * Dev: NuggaN85
- * Ghitub: NuggaN85
+ * Github: NuggaN85
  * Twitter: @NuggaN85
  * Copyright © 2015 All rights reserved.
  * Licensed under CC BY 3.0
