2ca8b8ecbcd5544d1fa15a99640ff6eff0edd3bd
NUGGAN - NGGG <NuggaN85@users.noreply.github.com>
2015-12-15 19:22:34 +0100

    Update protectionantiplagiat.php

--- protectionantiplagiat.php
+++ protectionantiplagiat.php
@@ -46,7 +46,7 @@ if($tentative > 0){
 	$write_here = fopen("protectionantiplagiat.cnx", "a"); // Fichier cnx auto inclus a la racine avec le protectionantiplagiat.php
 	fwrite($write_here, "\n" . $write_this);
 	fclose($write_here);
-	echo utf8_decode( '[Sécurité] Notre site web est protégé contre le vole et le spam, vos information sera automatiquement banni sur la base de donnée de projecthoneypot <br><br> [Information] : '.$navigateur.' '.get_ip().''); // Le texte que vous voulez que le voleur recevra dans les fichiers télécharger
+	echo utf8_decode( '[Sécurité] Notre site web est protégé contre le vole et le spam, vos information serons automatiquement bannies sur la base de donnée de projecthoneypot <br><br> [Information] : '.$navigateur.' '.get_ip().''); // Le texte que vous voulez que le voleur recevra dans les fichiers télécharger
 	
     //Notification emails
     $headers  = "From: <noreply@protection-anti-plagiat.com>\r\n";
